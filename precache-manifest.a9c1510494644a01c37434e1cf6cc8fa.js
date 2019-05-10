b4c5712fa2a371be04a2fac9fca6854585a94bae
lumenbarron <lucemendez@gmail.com>
2019-05-10 13:10:54 -0500

    Updates

---
+++ precache-manifest.a9c1510494644a01c37434e1cf6cc8fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e5e895440b1a596c584c03e59bc8c29",
+    "url": "/GDL002-burger-queen/index.html"
+  },
+  {
+    "revision": "f56f9657a50227717ab8",
+    "url": "/GDL002-burger-queen/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/GDL002-burger-queen/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "f56f9657a50227717ab8",
+    "url": "/GDL002-burger-queen/static/js/main.b0462ef4.chunk.js"
+  },
+  {
+    "revision": "f8c6079179fe30af807c",
+    "url": "/GDL002-burger-queen/static/js/runtime~main.2601196f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GDL002-burger-queen/static/media/logo.5d5d9eef.svg"
+  }
+]);
