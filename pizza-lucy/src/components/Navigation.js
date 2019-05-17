fa72df55cb32de97509ca0e50871205c5f2aba0c
lumenbarron <lucemendez@gmail.com>
2019-05-17 14:07:02 -0500

    adding the function of  hidden and show menus

--- pizza-lucy/src/components/Navigation.js
+++ pizza-lucy/src/components/Navigation.js
@@ -1,39 +1,50 @@
 import React from 'react';
+import {BreakfastCards} from './BreakfastCards';
+import {LunchCards} from './LaunchCards';
 
 export class Navigation extends React.Component {
+  constructor () {
+    super ()
+    this.state = { isHidden: false}
+  }
+
+  toggleHidden () {
+    this.setState({
+      isHidden: !this.state.isHidden
+    })
+  }
   render () {
       return (
-       
           <section className='row'> 
           <section className ='col-md-1'></section>
             <nav className="nav nav-pills nav-fill col-md-10">
               <a className="nav-item nav-link buttonsInicio" href="1.2"  id= "buttonInicio"> Inicio</a>
-              <a className="nav-item nav-link buttonsInicio" href="1.1" id= "buttonDesayuno">Desayuno</a>
+              <a onClick={this.toggleHidden.bind(this)} className="nav-item nav-link buttonsInicio" href="1.1" id= "buttonDesayuno">Desayuno</a>
               <a className="nav-item nav-link buttonsInicio" href="1" id= "buttonAlmuerzo" >Almuerzo</a>
               <a className="nav-item nav-link buttonsInicio" href="1.3" id= "buttonComandas"> Comandas </a>
             </nav>
+            {!this.state.isHidden && <LunchCards />}
             <section className ='col-md-1'></section>  
           </section>
-        
+
   )
   }
 }
 
 // export class Navigation extends React.Component {
-//     render () {
-//         return (
-//     <nav className="nav nav-pills flex-column flex-sm-row">
-//       <a className="flex-sm-fill text-sm-center nav-link " href="1.2">
-//       Inicio
-//       {this.props.title}
-//       </a>
-//       <a className="flex-sm-fill text-sm-center nav-link active" href="1.1">Desayuno</a>
-//       <a className="flex-sm-fill text-sm-center nav-link disabled" href="1"  aria-disabled="true">Almuerzo</a>
-//     </nav>
-//     )
-//     }
+//   render () {
+//       return (
+//           <section className='row'> 
+//           <section className ='col-md-1'></section>
+//             <nav className="nav nav-pills nav-fill col-md-10">
+//               <a className="nav-item nav-link buttonsInicio" href="1.2"  id= "buttonInicio"> Inicio</a>
+//               <a className="nav-item nav-link buttonsInicio" href="1.1" id= "buttonDesayuno">Desayuno</a>
+//               <a className="nav-item nav-link buttonsInicio" href="1" id= "buttonAlmuerzo" >Almuerzo</a>
+//               <a className="nav-item nav-link buttonsInicio" href="1.3" id= "buttonComandas"> Comandas </a>
+//             </nav>
+//             <section className ='col-md-1'></section>  
+//           </section>
+
+//   )
+//   }
 // }
-
-
-
-//export default Navigation
