2cee6582eabbc67f256818eaf3713820ec396799
lumenbarron <lucemendez@gmail.com>
2019-05-22 16:53:36 -0500

    adding firebase on the project

--- pizza-lucy/src/components/BreakfastCards.js
+++ pizza-lucy/src/components/BreakfastCards.js
@@ -2,15 +2,37 @@ import React from "react";
 import { desayuno } from "./desayuno.json";
 import { Tickets } from './Tickets';
 import {CustomerInput} from './CustomerInput';
+import firebase from './firebase';
+// import db from './firebase'
 
 export class BreakfastCards extends React.Component {
   constructor() {
     super();
     this.state = {
-      desayuno,
+      desayuno: [],
       orders: []
     };
   }
+
+  componentDidMount(){
+    const dbRef = firebase.database()
+    .ref().child('desayuno');
+    dbRef.on('value', snap => {
+      this.setState({
+        desayuno:snap.val()
+      })
+    })
+  }
+  // componentDidMount(){
+  //   db.collection('desayuno').get()
+  //   .then((snapShots) => {
+  //     this.setState ({
+  //       desayuno: snapShots.docs.map(doc => {
+  //         return {title: doc.data(), id:doc.id}
+  //       })
+  //     })
+  //   })
+  // }
 
 print = (event) => {
   const target=event.currentTarget;
@@ -27,7 +49,7 @@ print = (event) => {
   render() {
     const breakfast = this.state.desayuno.map((item, i) => {
       return (
-            <button className = " btn foodCards" name ={item.title} value={item.price} onClick={this.print}>
+            <button className = " btn foodCards" key = {i} name ={item.title} value={item.price} onClick={this.print}>
               {item.title} ${item.price}
             </button>
       );
--- pizza-lucy/src/components/firebase.js
+++ pizza-lucy/src/components/firebase.js
@@ -1,29 +1,30 @@
-import firebase from 'firebase/app';
-import 'firebase/firebase-firestore'
+ import firebase from 'firebase';
+// import firebase from 'firebase/app';
+// import 'firebase/firestore'
 
-firebase.initializeApp({
-    apiKey: "AIzaSyCQIQJpdfRSGZXr5jjR8pHXeEr4qoOiKi4",
-    authDomain: "il-piccolo-restaurante.firebaseapp.com",
-    databaseURL: "https://il-piccolo-restaurante.firebaseio.com",
-    projectId: "il-piccolo-restaurante",
-});
-let db = firebase.firestore();
-db.settings({timestampsInSnapshots : true })
-
-export default db;
-
-
-
-  //Configuration firebase
-// const firebaseConfig = {
+// firebase.initializeApp({
 //     apiKey: "AIzaSyCQIQJpdfRSGZXr5jjR8pHXeEr4qoOiKi4",
 //     authDomain: "il-piccolo-restaurante.firebaseapp.com",
 //     databaseURL: "https://il-piccolo-restaurante.firebaseio.com",
 //     projectId: "il-piccolo-restaurante",
-//     storageBucket: "il-piccolo-restaurante.appspot.com",
-//     messagingSenderId: "878022984897",
-//     appId: "1:878022984897:web:605481abd846ce77"
-//   };
-  // Initialize Firebase
-//   firebase.initializeApp(firebaseConfig);
-// export default firebase;
+// });
+// let db = firebase.firestore();
+// db.settings({timestampsInSnapshots : true })
+
+// export default db;
+
+
+
+//  Configuration firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCQIQJpdfRSGZXr5jjR8pHXeEr4qoOiKi4",
+    authDomain: "il-piccolo-restaurante.firebaseapp.com",
+    databaseURL: "https://il-piccolo-restaurante.firebaseio.com",
+    projectId: "il-piccolo-restaurante",
+    storageBucket: "il-piccolo-restaurante.appspot.com",
+    messagingSenderId: "878022984897",
+    appId: "1:878022984897:web:605481abd846ce77"
+  };
+//  Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+export default firebase;
